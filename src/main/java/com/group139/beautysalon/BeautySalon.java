b4cef642593dc691b4327070e99ad9c53da66022
IT25101747 <it25101747@my.sliit.lk>
2026-05-05 21:52:10 +0530

    update

---
+++ src/main/java/com/group139/beautysalon/BeautySalon.java
@@ -0,0 +1,12 @@
+package com.group139.beautysalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalon {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySalon.class, args);
+    }
+}
